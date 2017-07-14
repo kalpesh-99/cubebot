931b4e4f26fa0ff5a5a6a7a3c85b5bdb229a0ffb
kalpesh-99 <CARROT2@KALPESHs-MacBook.local>
2017-07-14 15:48:26 -0400

    working bot with FB Login Account Linking

---
+++ api_resources/getAttachment.py
@@ -0,0 +1,23 @@
+
+def getAttachment(attachment):
+    attachmentList = attachment
+    for item in attachmentList:
+        for key in item:
+            if key == 'type':
+                attachmentType = item['type']
+                if attachmentType == 'fallback':
+                    print("got fallback attachment type from getAttachment.py")
+                    attachmentTitle = item['title']
+                    attachmentText = "Got it, I'll file '%s' for safe keeping!" %attachmentTitle
+                elif attachmentType == 'template':
+                    print("got template attachment type")
+                    attachmentTitle = item['title']
+                    attachmentText = "Got it, I'll file '%s' for safe keeping!" %attachmentTitle
+
+                else:
+                    payload_url = item['payload']['url']
+                    attachmentText = "Ok, I'll save this attachment under: " +attachmentType
+                    print(payload_url)
+                print(attachmentType)
+        # receivedAttachment(attachmentText, sender_id)
+        return attachmentText
